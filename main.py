b98f40e4bb050a3d521aafe20c9b8d13208935cc
Game K <hack.h@laposte.net>
2023-11-19 21:38:10 +0100

    Réorganisation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from threading import Thread
-from tkinter import PhotoImage, Tk, LabelFrame, StringVar, Label, Entry, Spinbox, Button, filedialog
-import requests
+from core import Core
+from tkinter import PhotoImage, Tk, LabelFrame, StringVar, Label, Entry, Spinbox, Button, filedialog, messagebox
 import datetime
+import requests
+import urllib.parse
+import utils
 import time
 import cv2
 import re
@@ -31,29 +34,6 @@ def update_image(file):
     img2 = PhotoImage(file=path)
     video.configure(image=img2)
     video.image = img2
-
-def humanbytes(B):
-    """
-    https://stackoverflow.com/questions/12523586/python-format-size-application-converting-b-to-kb-mb-gb-tb#answer-31631711
-
-    Return the given bytes as a human friendly KB, MB, GB, or TB string.
-    """
-    B = float(B)
-    KB = float(1024)
-    MB = float(KB ** 2) # 1,048,576
-    GB = float(KB ** 3) # 1,073,741,824
-    TB = float(KB ** 4) # 1,099,511,627,776
-
-    if B < KB:
-        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
-    elif KB <= B < MB:
-        return '{0:.2f} KB'.format(B / KB)
-    elif MB <= B < GB:
-        return '{0:.2f} MB'.format(B / MB)
-    elif GB <= B < TB:
-        return '{0:.2f} GB'.format(B / GB)
-    elif TB <= B:
-        return '{0:.2f} TB'.format(B / TB)
 
 def _check_var():
     ok = True
@@ -87,7 +67,7 @@ def _time(path):
 
 def _size(path):
     s = sum([os.path.getsize(os.path.join(path, i)) for i in os.listdir(path) if i.endswith(".ts")])
-    size_value.set(humanbytes(s))
+    size_value.set(utils.humanbytes(s))
     l_size.update()
 
 def _exet(val):
@@ -97,94 +77,90 @@ def _exet(val):
 def download(channel_name, id_vid):
     global is_start, out_dir_path
     nami = 0
-    # Renseignez votre client ID Twitch, votre secret client Twitch et le nom de la chaîne Twitch que vous souhaitez regarder
-    client_id = "kimne78kx3ncx6brgo4mv6wki5h1ko"
+
     access_token = apikey_value.get()
     out_dir_path = os.path.join(out_dir_path, e_username.get(), i_vidname.get())
 
     _status(True)
-    nberr = 0
     ultim = []
 
     while is_start:
-        try:
-            st = time.time()
+        st = time.time()
 
-            res = requests.post(
-                "https://gql.twitch.tv/gql", 
-                headers={
-                    "Client-ID": client_id, 
-                    "Authorization": "OAuth " + access_token
-                }, 
-                json={
-                    "operationName":"PlaybackAccessToken_Template", 
-                    "query":"query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}", 
-                    "variables": {
-                        "isLive":True, 
-                        "login":channel_name, 
-                        "isVod":False, 
-                        "vodID":"", 
-                        "playerType":"site"
-                    }
-                },
-                verify=False).json()
+        Core.ACCESS_REQUEST["variables"]["login"] = channel_name
 
-            token = res["data"]["streamPlaybackAccessToken"]["value"]
-            sig = res["data"]["streamPlaybackAccessToken"]["signature"]
+        res = requests.post(
+            url="https://gql.twitch.tv/gql", 
+            headers={
+                "Client-ID": Core.CLIENT_ID, 
+                "Authorization": f"OAuth {access_token}"
+            }, 
+            json=Core.ACCESS_REQUEST,
+            verify=Core.VERIFY
+        ).json()
 
-            # Obtenez l'URL de lecture en direct
-            url = f"https://usher.ttvnw.net/api/channel/hls/{channel_name}.m3u8?player=twitchweb&token={token}&sig={sig}&allow_source=true"
-            res = requests.get(url, verify=False)
-            m3u8 = res.content.decode("utf8")
+        res = res["data"]["streamPlaybackAccessToken"]
 
-            # search best quality
-            quality_index = None
-            quality_value = 0
-            for index, line in enumerate(m3u8.split("\n")):
-                if line.startswith("#EXT-X-STREAM-INF"):
-                    quality = re.search('RESOLUTION=(.*),CODECS', line, re.IGNORECASE).group(1)
-                    quality = quality.split("x")
-                    quality = int(quality[0])*int(quality[1])
-                    if quality > quality_value:
-                        quality_value = quality
-                        quality_index = index
-            # best quality and +1 to find stream url
-            url = m3u8.split("\n")[quality_index+1]
+        if res is not None:
+            token = res["value"]
+            token = urllib.parse.quote(token)
+            sig = res["signature"]
+        else:
+            _status(False)
+            is_start = False
+            messagebox.showwarning("Streameur/se non trouvé", "Ce streameur/se n'a pas été trouvé")
+            break
 
-            res = requests.get(url, verify=False)
-            res = res.content.decode("utf8").split("\n")
+        # Obtenez l'URL de lecture en direct
+        res = requests.get(
+            url=f"https://usher.ttvnw.net/api/channel/hls/{channel_name}.m3u8?player=twitchweb&token={token}&sig={sig}&allow_source=true", 
+            verify=Core.VERIFY)
+        m3u8 = res.content.decode("utf8")
 
-            url_list = [res[index+1] for index, line in enumerate(res) if line.startswith("#EXTINF:")]
+        if "transcode_does_not_exist" in m3u8:
+            _status(False)
+            is_start = False
+            messagebox.showwarning("Live non trouvé", "Ce streameur/se n'est plus en live")
+            break
 
-            for i in url_list:
-                res = requests.get(i, verify=False).content
-                if res not in ultim[-15:]:
-                    nami += 1
-                    ultim.append(res)
-                    if not os.path.exists(out_dir_path):
-                        os.makedirs(out_dir_path)
-                    open(f"{out_dir_path}/{channel_name}_{nami}.ts", "wb").write(res)
+        # search best quality
+        quality_index = None
+        quality_value = 0
+        for index, line in enumerate(m3u8.split("\n")):
+            if line.startswith("#EXT-X-STREAM-INF"):
+                quality = re.search('RESOLUTION=(.*),CODECS', line, re.IGNORECASE).group(1)
+                quality = quality.split("x")
+                quality = int(quality[0])*int(quality[1])
+                if quality > quality_value:
+                    quality_value = quality
+                    quality_index = index
+        # best quality and +1 to find stream url
+        url = m3u8.split("\n")[quality_index+1]
 
-            ultim = ultim[-150:]
-            _exet(time.time()-st)
-            Thread(target=lambda:update_image(f"{out_dir_path}/{channel_name}_{nami}.ts")).start()
-            nberr = 0
-            _time(out_dir_path)
-            _size(out_dir_path)
+        res = requests.get(url, verify=Core.VERIFY)
+        res = res.content.decode("utf8").split("\n")
 
-            for _ in range(int(waittime_value.get())):
-                time.sleep(1)
-                if not is_start:
-                    break
+        url_list = [res[index+1] for index, line in enumerate(res) if line.startswith("#EXTINF:")]
 
-        except Exception as e:
-            err = "unsupported operand type(s) for +: 'NoneType' and 'int'"
-            if str(e) == err:
-                nberr += 1
-                time.sleep(1)
-            if nberr >= 10:
-                _status(False)
-                is_start = False
+        for i in url_list:
+            res = requests.get(i, verify=Core.VERIFY).content
+            if res not in ultim[-15:]:
+                nami += 1
+                ultim.append(res)
+                if not os.path.exists(out_dir_path):
+                    os.makedirs(out_dir_path)
+                open(f"{out_dir_path}/{channel_name}_{nami}.ts", "wb").write(res)
+
+        ultim = ultim[-150:]
+        _exet(time.time()-st)
+        Thread(target=lambda:update_image(f"{out_dir_path}/{channel_name}_{nami}.ts")).start()
+        _time(out_dir_path)
+        _size(out_dir_path)
+
+        for _ in range(int(waittime_value.get())):
+            time.sleep(1)
+            if not is_start:
+                break
         
     _status(False)
 
@@ -258,11 +234,13 @@ out_value = StringVar()
 out_value.set("empty")
 l_out = Label(root, textvariable=out_value)
 l_out.place(x=130, y=268)
+
 def _browse_file(search=False):
     global out_dir_path
     if search == True:
         out_dir_path = filedialog.askdirectory()
     _update_out()
+
 b_browse = Button(root, text="Browse", command=lambda: _browse_file(True), width=10)
 b_browse.place(x=340, y=265)
 e_username.bind("<FocusOut>", _browse_file)
@@ -272,11 +250,13 @@ def _stop():
     is_start = False
 b_exit = Button(root, text="Stop", command=_stop, width=15)
 b_exit.place(x=440, y=265)
+
 def _start():
     global is_start
     if not is_start and _check_var():
         is_start = True
         Thread(target=lambda:download(username_value.get(), vidname_value.get())).start()
+
 b_exit = Button(root, text="Download", command=_start, width=15)
 b_exit.place(x=570, y=265)
 
